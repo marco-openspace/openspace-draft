8b005d8391013e0a685cbc37189e082d45278b1f
Marco Openspace <149870435+marco-openspace@users.noreply.github.com>
2023-11-04 15:03:12 +0100

    +

--- next.config.mjs
+++ next.config.mjs
@@ -18,8 +18,6 @@ export default bundleAnalyzer({
     loader: 'akamai',
     path: '',
   },
-  basePath: '/nextjs-pages',
-  assetPrefix: '/nextjs-pages',
   webpack: (config) => {
     // config.externals is needed to resolve the following errors:
     // Module not found: Can't resolve 'bufferutil'

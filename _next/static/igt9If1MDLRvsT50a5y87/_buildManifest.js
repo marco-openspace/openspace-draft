b1437970756957fe365dc7d0e7156ed55227f766
marco-openspace <149870435+marco-openspace@users.noreply.github.com>
2023-11-04 10:43:19 +0000

    Deploying to openspace-draft from @ marco-openspace/openspace-draft@a2ec07d27057f6202cc4165eecc6546133c9d3ae 🚀

---
+++ _next/static/igt9If1MDLRvsT50a5y87/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/821-14a9f58a6d6d52d7.js","static/chunks/pages/index-006b6089c60517b9.js"],"/_error":["static/chunks/pages/_error-65ac7445167ddb62.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
